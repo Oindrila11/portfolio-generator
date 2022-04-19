cad1783d45530911aafc2b9402bb80ba7e2b6b16
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-04-19 13:14:26 -0500

    generated html file

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [oindrila, oindrila11] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+fs.writeFile('./index.html', generatePage(oindrila, oindrila11), err => {
+  if (err) throw new Error (err);
 
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
