fd6f95a6b5bdf185514288596ee3fe971e94d04d
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-04-20 23:59:30 -0500

    added questions

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [oindrila, oindrila11] = profileDataArgs;
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile('./index.html', generatePage(oindrila, oindrila11), err => {
-  if (err) throw new Error (err);
+// const pageHTML = generatePage(oindrila, oindrila11);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
